ad4e747278641107bf5cb9ec062f3b109b87e9c9
sun-yasha <167542533+sun-yasha@users.noreply.github.com>
2025-01-21 12:41:33 +0500

    Add files via upload

---
+++ Scripts/MaxScore.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MaxScore : MonoBehaviour
+{
+    
+    void Start()
+    {
+        GetComponent<Text>().text = PlayerPrefs.GetInt("Score").ToString();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/RightOne.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightOne : MonoBehaviour
+{
+    private GameObject mainCube;
+
+    void Start()
+    {
+        mainCube = GameObject.Find("Main Cube");
+    }
+
+    void OnMouseDown()
+    {
+        if (GetComponent<Renderer>().material.color == mainCube.GetComponent<Renderer>().material.color)
+            mainCube.GetComponent<GameCntrl>().next = true;
+        else
+            mainCube.GetComponent<GameCntrl>().lose = true;
+    }
+}
